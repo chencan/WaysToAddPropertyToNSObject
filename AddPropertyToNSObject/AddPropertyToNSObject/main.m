af0098b71cfb13bad6e22f26cd90a81d3bf96af4
chencan <chencanjob@gmail.com>
2012-07-21 10:57:54 +0800

    init

---
+++ AddPropertyToNSObject/AddPropertyToNSObject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AddPropertyToNSObject
+//
+//  Created by Can Chen on 12-7-21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
